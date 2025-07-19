40d849ec239491aafbd428646c1bf06d1c7f5e49
b-sandrine <sanrdushimimana@gmail.com>
2025-07-18 23:39:57 -0700

    updated

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
+
 """Reddit API subscriber counter"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
+    
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
         'User-Agent': 'python:sub.count.script:v1.0 (by /u/your_reddit_username)'
     }
 
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
+        response = requests.get(url, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
             return data.get('data', {}).get('subscribers', 0)
