8b0d72b8e235034ef90fa05aa8efe127f77d28f2
b-sandrine <sanrdushimimana@gmail.com>
2025-07-18 23:18:13 -0700

    Documented the module

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
+
+""" 0-subs """
+
 import requests
 
 def number_of_subscribers(subreddit):
     """Return the number of subscribers for a given subreddit."""
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'MyRedditBot/1.0'}
+    headers = {
+            'User-Agent': 'MyRedditScript/1.0 (by u/Flaky_Party_2046)'
+            }
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
+        print(f"The response from reddit: {response}")
         if response.status_code == 200:
             data = response.json()
             return data.get('data', {}).get('subscribers', 0)
