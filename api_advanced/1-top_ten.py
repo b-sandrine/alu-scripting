88d00fa418a6a9a644b12cb03ab3ad1815d60fa0
b-sandrine <sanrdushimimana@gmail.com>
2025-07-29 01:23:34 -0700

    updates

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -9,15 +9,12 @@ import requests
 
 def top_ten(subreddit):
     """"top ten"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
-        .format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
 
-    res = requests.get(url,
-                       headers={
-                           'User-Agent': 'Mozilla/5.0'})
+    res = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
 
     if res.status_code != 200:
-        print("OK")
+        print("OK", end="")  # <-- Fix: suppress the newline
     else:
         json_response = res.json()
         posts = json_response.get('data').get('children')
